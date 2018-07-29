b3d8e01bf5a5bcb827a527de8ed39cd06c177ace
Arpit <rpittrer@gmail.com>
2018-07-29 15:10:04 +0530

    deplyements

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,15 @@
-var Driver = artifacts.require('./Driver.sol');
-var expo = artifacts.require('./Shipper_Exporter.sol');
-var impo = artifacts.require('./Shipper_Importer.sol');
+var vendor = artifacts.require('./vendor.sol');
+var inspector = artifacts.require('./inspector.sol');
+var shipper = artifacts.require('./shipper.sol');
+var vessel = artifacts.require('./vessel.sol');
+var ownable = artifacts.require('./ownable.sol');
+var eventDriver = artifacts.require('./evenDriver.sol');
 
 module.exports = function(deployer) {
-    deployer.deploy(Driver);
-    deployer.deploy(expo);
-    deployer.deploy(impo);
+    deployer.deploy(vendor);
+    deployer.deploy(inspector);
+    deployer.deploy(shipper);
+    deployer.deploy(vessel);
+    deployer.deploy(ownable);
+    deployer.deploy(eventDriver);
 };
