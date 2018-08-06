fbcf5da372f73585e77212615d6e2d4479a3268b
Arpit <rpittrer@gmail.com>
2018-08-06 19:51:52 +0530

    hbs working

--- server.js
+++ server.js
@@ -3,24 +3,22 @@ const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-const Vendor = {
-    details: [
-        {
-            shipmentId: 123,
-            description: 'sddd',
-            location: 'O',
-            date: 'ssdf',
-            time: 'ssdff'
-        },
-        {
-            shipmentId: 134,
-            description: 'gihdind',
-            location: 'D',
-            date: 'sgggg',
-            time: 'kmkkd'
-        }
-    ]
-};
+const Vendor = [
+    {
+        shipmentId: 123,
+        description: 'sddd',
+        location: 'O',
+        date: 564,
+        time: 789
+    },
+    {
+        shipmentId: 134,
+        description: 'gihdind',
+        location: 'D',
+        date: 899,
+        time: 786576
+    }
+];
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
@@ -29,23 +27,6 @@ app.set('view engine', 'hbs');
 
 hbs.registerHelper('currentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', text => text.toUpperCase());
-
-hbs.registerHelper('list', vendor => {
-    var out = '';
-   // console.log(vendor);
-    for (var i = 0; i < vendor.details.length; i++) {
-        out =
-            out +
-            '<tr>' +
-            `<th scope="row"> ${vendor.details[i].shipmentId} </th>
-            <td> ${vendor.details[i].description} </td>]
-            <td> ${vendor.details[i].location} </td>
-            <td> ${vendor.details[i].date} </td>
-            <td> ${vendor.details[i].time} </td>` +
-            '</tr>';
-    }
-    return out;
-});
 
 app.use((req, res, next) => {
     var now = new Date().toString();
@@ -63,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-    res.render('index.hbs', Vendor);
+    res.render('index.hbs', {Vendor});
 });
 app.get('/bad', (req, res) => {
     var error = {
--- src/js/a.js
+++ src/js/a.js
@@ -1,35 +1,34 @@
-const Vendor = {
-    details: [
-        {
-            shipmentId: 123,
-            description: 'sddd',
-            location: 'O',
-            date: 'ssdf',
-            time: 'ssdff'
-        },
-        {
-            shipmentId: 134,
-            description: 'gihdind',
-            location: 'D',
-            date: 'sgggg',
-            time: 'kmkkd'
-        }
-    ]
-};
+const Vendor = [
+    {
+        shipmentId: 123,
+        description: 'sddd',
+        location: 'O',
+        date: 'ssdf',
+        time: 'ssdff'
+    },
+    {
+        shipmentId: 134,
+        description: 'gihdind',
+        location: 'D',
+        date: 'sgggg',
+        time: 'kmkkd'
+    }
+];
 
-registerHelper = (vendor => {
+registerHelper = vendor => {
     var out = '';
-    for (var i = 0; i < vendor.details.length; i++) {
-        out = out +
+    for (var i = 0; i < vendor.length; i++) {
+        out =
+            out +
             '<tr>' +
-            `<th scope="row"> ${vendor.details[i].shipmentId} </th>
-            <td> ${vendor.details[i].description} </td>]
-            <td> ${vendor.details[i].location} </td>
-            <td> ${vendor.details[i].date} </td>
-            <td> ${vendor.details[i].time} </td>` +
+            `<th scope="row"> ${vendor[i].shipmentId} </th>
+            <td> ${vendor[i].description} </td>]
+            <td> ${vendor[i].location} </td>
+            <td> ${vendor[i].date} </td>
+            <td> ${vendor[i].time} </td>` +
             '</tr>';
     }
     return out;
-});
+};
 
 console.log(registerHelper(Vendor));
