ef86005ded1d4b73dcfa81bea04ef288c7bed80d
Archit Aggarwal <archit.aggarwal1998@gmail.com>
2018-08-07 17:30:40 +0530

    data

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,33 +1,61 @@
 const Vendor = [
     {
         shipmentId: 123,
-        description: 'sddd',
-        location: 'O',
-        date: 564,
-        time: 789
+        description: 'NOR Tendered',
+        location: 'Origin',
+        date: 07/07/16,
+        time: 2116
     },
     {
         shipmentId: 134,
-        description: 'gihdind',
-        location: 'D',
-        date: 899,
-        time: 786576
+        description: 'NOR Tendered',
+        location: 'Destination',
+        date: 03/08/16,
+        time: 1123
+    },
+    {
+        shipmentId: 145,
+        description: 'Vessel Sailed',
+        location: 'Destination',
+        date: 05/08/16,
+        time: 1200
+    },
+    {
+        shipmentId: 845,
+        description: 'Vessel Sailed',
+        location: 'Origin',
+        date: 04/07/18,
+        time: 0100
     }
 ];
 const Vessel = [
     {
         shipmentId: 234,
-        description: 'sddd',
-        location: 'dff',
-        date: 9088,
-        time: 654
+        description: 'NOR Tendered',
+        location: 'Origin',
+        date: 12/12/17,
+        time: 0236
     },
     {
         shipmentId: 6434,
-        description: '7554',
-        location: 'D',
-        date: 899,
-        time: 786576
+        description: 'NOR Tendered',
+        location: 'Destination',
+        date: 21/04/17,
+        time: 1309
+    },
+    {
+        shipmentId: 4325,
+        description: 'Vessel Sailed',
+        location: 'Destination',
+        date: 30/03/18,
+        time: 1703
+    },
+    {
+        shipmentId: 7890,
+        description: 'Vessel Sailed',
+        location: 'Origin',
+        date: 15/11/17,
+        time: 0908
     }
 ];
 const Inspector = [];
