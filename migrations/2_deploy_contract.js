30251e0d4c4b3b9395b328f3c46a98b4397df6e5
Arpit <rpittrer@gmail.com>
2018-08-07 14:03:41 +0530

    build generated

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,27 +1,15 @@
 var vendor = artifacts.require('./vendor.sol');
 var inspector = artifacts.require('./inspector.sol');
 var shipper = artifacts.require('./shipper.sol');
-<<<<<<< HEAD
-var shipowner = artifacts.require('./shipowner.sol');
-var ownable = artifacts.require('./ownable.sol');
-var eventDriver = artifacts.require('./evenDriver.sol');
-=======
 var vessel = artifacts.require('./vessel.sol');
 var loadingPort = artifacts.require('./loadingPort.sol');
 var dischargePort = artifacts.require('./dischargePort.sol');
->>>>>>> 8c605404f26107875cab90b984a1f62b0ca1e840
 
 module.exports = (deployer) => {
     deployer.deploy(vendor);
     deployer.deploy(inspector);
     deployer.deploy(shipper);
-<<<<<<< HEAD
-    deployer.deploy(shipowner);
-    deployer.deploy(ownable);
-    deployer.deploy(eventDriver);
-=======
     deployer.deploy(vessel);
     deployer.deploy(loadingPort);
     deployer.deploy(dischargePort);
->>>>>>> 8c605404f26107875cab90b984a1f62b0ca1e840
 };
